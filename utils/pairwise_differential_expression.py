1375425f7e811e12eecd5ef39ce82096dee075c3
Elgene Quitevis <quitevisejohn@gmail.com>
2023-11-21 13:00:20 -0800

    updated the tutorial notebooks, added backup url to download data, updated pairwise utils functions and notebooks -- Diff Exp keys now ommited from being included into the anndata

--- utils/pairwise_differential_expression.py
+++ utils/pairwise_differential_expression.py
@@ -48,6 +48,7 @@ def pairwise_differential_analysis(adata, target_cluster, leiden_name, min_logfc
         Performs a pairwise comparison for a single target cluster. If there are N clusters this 
         function will perform a pairwise comparison N-1 times iteratively. 
     [Parameters]
+            adata          : your anndata object
             target_cluster : A target cluster/group of interest within the columns containing
                              clusters/groups within the AnnData.obs metadata of an anndata object
             leiden_name    : The column name or key containing cluster/groups
@@ -60,18 +61,25 @@ def pairwise_differential_analysis(adata, target_cluster, leiden_name, min_logfc
     reference_clusters = adata.obs.loc[adata.obs[leiden_name] != target_cluster][leiden_name].unique()
     target_cluster_results = []
     for cluster_reference in reference_clusters:
+        # key labels for the differential expression analysis -- used for accesing the
+        # current pairwise results
+        comparison_key = f'target_cluster_{target_cluster}vs{cluster_reference}'
+        
+        # perform differential expression analysis to get cluster specific genes for target cluster vs
+        # a reference cluster (another cluster not the target cluster)
         sc.tl.rank_genes_groups(
             adata,
             use_raw=False,
-            key_added=f'target_cluster_{target_cluster}vs{cluster_reference}',
+            key_added=comparison_key,
             groupby=leiden_name,
             groups=[target_cluster], 
             reference=cluster_reference,
         )
         
+        # extract the result -- get the dataframe
         de_results = sc.get.rank_genes_groups_df(
             adata, 
-            key=f'target_cluster_{target_cluster}vs{cluster_reference}', 
+            key=comparison_key, 
             group=[target_cluster], 
             log2fc_min=min_logfc,
             pval_cutoff=pval_cutoff,
@@ -81,7 +89,12 @@ def pairwise_differential_analysis(adata, target_cluster, leiden_name, min_logfc
         de_results['group'] = de_results.shape[0] * [target_cluster]
         de_results['compared_against'] = de_results.shape[0] * [cluster_reference]
         
+        # append the results
         target_cluster_results.append(de_results)
+        
+        # delete the key that was added -- this should declutter the anndat.uns object
+        del adata.uns[comparison_key]
+        
     return target_cluster_results
 
 
@@ -207,44 +220,9 @@ def plot_pairwise_results(adata, target_cluster, save=None):
 
     # Save and show 
     if save is not None:
-        subplot.write_image(save)
+        subplot.write_image(f'{save}.png')
+        subplot.write_html(f'{save}.html')
 
     return subplot
 
-# Computing linkage for hierarchical clustering
-def compute_linkage(data_obsm, data_obs_index, data_obs_label, feature_names, 
-                    dist_metric='cosine', linkage_dist_metric='euclidean', linkage_method='average'):
-    # params: data, feature_names, obs_label, metric, linkage_method
-    '''
-        [Summary]
-            Computes a linkages to be used for constructing dendrograms. Use linkage returned
-            with scipy.cluster.hierarchy.dendrogram function to visualize results
-        [Parameters]
-            data_obsm           : The features and values to be used to compute linkages e.g. gene expression values, PCs
-            data_obs_index      : The designated name or index for each of the sample rows in data_obsm
-            data_obs_label      : An added categorical label column to -- used for grouping the rows together
-            feature_names       : The column names or names of the features
-            dist_metric         : The distance metric to use e.g. cosine/euclidean etc. see sklearn.metrics.pairwise_distances documentation
-            linkage_dist_metric : The distance metric used for linkage method see scipy.cluster.hierarchy.linkage documentation 
-            linkage_method      : Linkage method used to compute linkages e.g. single, average, complete etc.
-        [Returns]
-            Returns the computed linkages, group_labels to be used for dendrogram as well as the 
-            paired distance matrix for each group (unordered/unclustered matrix)
-    '''
-    # build the data matrix and add associated group labels
-    features_matrix = pd.DataFrame(data_obsm, index=data_obs_index, columns=feature_names)
-    features_matrix.loc[:, 'clust_labels'] = data_obs_label
-    
-    # compute the mean of the values per label in the data set and create a new matrix
-    features_mean = features_matrix.groupby('clust_labels').mean()
-    
-    # compute the pairwise distances of the groups in the data
-    features_dist = pd.DataFrame(
-        pairwise_distances(features_mean, metric=dist_metric), 
-        index=features_mean.index, columns=features_mean.index)
-    
-    # compute the linkages 
-    compute_linkage = scipy.cluster.hierarchy.linkage(features_dist, method=linkage_method, metric=linkage_dist_metric)
-    
-    return compute_linkage, features_mean.index, features_dist
 
